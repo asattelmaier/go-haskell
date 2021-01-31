5b1ae2f71a4b8b19e7263904e4fd1728cc1d8c99
Alexander Sattelmaier <alexander@sattelmaier.info>
2021-01-31 10:07:56 +0100

    Add player json parsing

--- src/API/JSON.hs
+++ src/API/JSON.hs
@@ -6,31 +6,48 @@ module API.JSON (main) where
 
 
 import Data.Aeson
-import Data.Text
 import qualified Data.ByteString.Lazy.Char8 as BL
+import Go.Board
 
 
 
-data Game = Game { activePlayer :: Text, passivePlayer :: Text }
-            deriving (Show)
-             
+data Game = Game
+            { activePlayer :: Color
+            , passivePlayer :: Color
+            } deriving (Show)
+
+
+
+instance ToJSON Color where
+  toJSON Black = String "Black"
+  toJSON White = String "White"
+
+
+
+instance FromJSON Color where
+  parseJSON color = case color of
+    "Black" -> pure Black
+    "White" -> pure White
+    _       -> fail $ "Unknwon Color: " <> show color
+
 
 
 instance ToJSON Game where
   toJSON (Game activePlayer passivePlayer) = object [ "activePlayer"  .= activePlayer,
                                                       "passivePlayer" .= passivePlayer ]
 
-  toEncoding Game{..} = pairs $
-        "activePlayer"  .= activePlayer <>
-        "passivePlayer" .= passivePlayer
 
+
+  toEncoding Game{..} = pairs $
+    "activePlayer"  .= activePlayer <>
+    "passivePlayer" .= passivePlayer
 
 
 
 instance FromJSON Game where
   parseJSON (Object v) = Game <$>
-                         v .: "activePlayer" <*>
-                         v .: "passivePlayer"
+    v .: "activePlayer" <*>
+    v .: "passivePlayer"
 
 
 
@@ -39,5 +56,5 @@ main = do
   -- TODO: Just a test, add implementation
   print $ (decode "{\"activePlayer\": \"Black\",\"passivePlayer\":\"White\"}" :: Maybe Game)
   
-  BL.putStrLn (encode (Game "Black" "White"))
+  BL.putStrLn (encode (Game Black White))
 
