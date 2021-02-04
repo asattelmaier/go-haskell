b1e54ab5e67c5a7dfda295d701cdb6950e2381f8
Alexander Sattelmaier <alexander@sattelmaier.info>
2021-02-04 19:04:16 +0100

    Add play json parsing

--- src/API/JSON.hs
+++ src/API/JSON.hs
@@ -117,11 +117,61 @@ instance FromJSON Game where
 
 
 
+
+-------------------------------------------------------------------------------
+-- Command
+-------------------------------------------------------------------------------
+
+data Command = PlayStone | Pass deriving (Show)
+
+
+
+instance ToJSON Command where
+  toJSON PlayStone = String "PlayStone"
+  toJSON Pass      = String "Pass"
+
+
+
+instance FromJSON Command where
+  parseJSON command = case command of
+    "PlayStone" -> pure PlayStone
+    "Pass"      -> pure Pass
+    _           -> fail $ "Unknwon Command: " <> show command
+
+
+
+-------------------------------------------------------------------------------
+-- Play
+-------------------------------------------------------------------------------
+
+data Play = Play { game :: Game
+                 , command :: Command
+                 } deriving (Show)
+
+
+
+instance ToJSON Play where
+  toJSON (Play game command) =
+    object [ "game"    .= game,
+             "command" .= command ]
+
+  toEncoding Play{..} = pairs $
+    "game"    .= game <>
+    "command" .= command
+
+
+
+instance FromJSON Play where
+  parseJSON (Object v) = Play
+    <$> v .: "game"
+    <*> v .: "command"
+
+
+
 main :: IO ()
 main = do
   -- TODO: Just a test, add implementation
-  print (decode "{\"positions\":[[[{\"location\": {\"x\": 2, \"y\": 1}, \"state\": \"Black\"}]]],\"activePlayer\": \"Black\",\"passivePlayer\":\"White\"}" :: Maybe Game)
+  print (decode "{\"game\":{\"positions\":[[[{\"location\": {\"x\": 2, \"y\": 1}, \"state\": \"Black\"}]]],\"activePlayer\": \"Black\",\"passivePlayer\":\"White\"},\"command\":\"Pass\"}" :: Maybe Play)
   
-  let positions = [createBoard 2]
-  BL.putStrLn (encode (Game positions Black White))
+  BL.putStrLn (encode (Play (createGame 2) PlayStone))
 
