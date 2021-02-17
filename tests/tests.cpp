337a461a0dde5eaff22dc16cda2c74ed61e8ce57
Alexander Sattelmaier <alexander@sattelmaier.info>
2021-02-17 19:36:37 +0100

    Add Rule 3 Test

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <nlohmann/json.hpp>
 #include "utils/go_json_api.h"
+#include <iostream>
 
 
 
@@ -13,10 +14,10 @@ using json = nlohmann::json;
 // Go is a game between two players, called Black and White
 
 TEST(Tests, Rule1){
-  json newGame = json::object({ {"command", "NewGame"} });
+  json createNewGame = json::object({ {"command", "NewGame"} });
   
-  json game = play(newGame)["game"];
-  
+  json game = play(createNewGame)["game"];
+
   ASSERT_EQ(game["activePlayer"].get<string>(),"Black");
   ASSERT_EQ(game["passivePlayer"].get<string>(), "White");
 }
@@ -27,11 +28,28 @@ TEST(Tests, Rule1){
 // Go is played on a plain grid of 19 horizontal and 19 vertical lines, called a board.
 
 TEST(Tests, Rule2){
-  json newGame = json::object({ {"command", "NewGame"} });
+  json createNewGame = json::object({ {"command", "NewGame"} });
   
-  json board = play(newGame)["game"]["positions"][0];
+  json board = play(createNewGame)["game"]["positions"][0];
 
   ASSERT_EQ(board.size(), 19);
   ASSERT_EQ(board[0].size(), 19);
+}
+
+
+
+// Rule 3
+// Go is played with playing tokens known as stones.
+
+TEST(Tests, Rule3){
+  json createNewGame = json::object({ {"command", "NewGame"} });
+  json newGame = play(createNewGame);
+
+  newGame["location"] = json::object({ {"x", 0}, {"y", 0} });
+  newGame["command"] = "PlayStone";
+  json interception = play(newGame)["game"]["positions"][0][0][0];
+
+  // TODO: Update API, remove "state" and "location"
+  ASSERT_EQ(interception["state"], "Black");
 }
 
