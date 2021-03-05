4cf3ff862525e185329a8eaa4b5cb30aa40d0f5c
Alexander Sattelmaier <alexander@sattelmaier.info>
2021-03-05 20:01:51 +0100

    Update cabal

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 ## Prerequisites
 
-* ghc   >= 8.4.4
-* cabal >= 1.10
+* ghc   >= 8.10.4
+* cabal >= 2.4
 * conan >= 1.33.1
 * cmake >= 2.8.12
 
@@ -22,7 +22,7 @@ cabal build
 ## Run CLI Game
 
 ```bash
-cabal run -- -m cli
+cabal run :go-haskell -- -m cli
 ```
 
 
--- tests/conanfile.py
+++ tests/conanfile.py
@@ -9,30 +9,23 @@ current_path = os.getcwd()
 
 
 class GoHaskell:
-    app_name = "go-haskell"
     build_command = "cabal build"
-    run_command = "-m json"
+    run_command = "cabal run -v0 :go-haskell -- -m json"
 
     @staticmethod
     def get_path():
         return os.path.normpath(current_path + 2 * (os.pardir + os.sep))
 
-    @staticmethod
-    def get_app_path():
-        return "{0}{1}dist{1}build{1}go-haskell".format(GoHaskell.get_path(), os.sep)
 
 
-
-class GoHaskellJsonApiTest(ConanFile):
+class TestGoHaskellJsonApi(ConanFile):
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
     requires = ["gtest/1.10.0", "nlohmann_json/3.9.1"]
     test_app_name = "go_haskell_json_api_test"
     environment_variables = {
-        "EXECUTE_GO_HASKELL_COMMAND": "\"cd %s && .%s%s %s \"" % (
-            GoHaskell.get_app_path(),
-            os.sep,
-            GoHaskell.app_name,
+        "EXECUTE_GO_HASKELL_COMMAND": "\"cd %s && %s \"" % (
+            GoHaskell.get_path(),
             GoHaskell.run_command
         ),
     }
